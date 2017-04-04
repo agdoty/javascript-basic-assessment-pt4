653af02f6d58514b1210f22e7d60596b8d66b02e
atrevisanut <atrevisanut@gmail.com>
2017-04-04 16:24:03 -0600

    answers

--- test.js
+++ test.js
@@ -12,6 +12,13 @@ var candies = {
   mugrootbeer: 'not candy... but the best'
 };
 
+for(var key in candies){
+  if(key.charAt(0) === 'm'){
+    console.log(this.key + "  hit")
+    candies[key] =  "no m&m's for you";
+  }
+}
+
 /////////////
 // Num. 8  //////////////////////////////////////////////////////////
 // Using the array below, do the following:                        //
@@ -19,12 +26,12 @@ var candies = {
  var colors = ['red', 'blue', 'green', 'yellow'];
 
 // (A) Add the color 'purple' to the end of the array              //
-
+ colors.push('purple');
 // (B) Remove 'red' and store it in a variable called colorRed     //
-
+ var colorRed = colors.splice(0,1)[0];
 // (C) Add 'pink' to the beginning of the colors array             //
-
+ colors.unshift('pink');
 // (D) Leave 'green' in the array, but save it into another        //
 // variable called colorGreen                                      //
-
+ var colorGreen = colors.slice(2,3)[0];
 /////////////////////////////////////////////////////////////////////
